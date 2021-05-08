a9453b4ab1e4f4c5e7a95f839e4e155afacc5ce4
Sumer Kumar <sumer47kumar@gmail.com>
2021-05-08 17:11:31 +0530

    did single source shortest path of tree in from of graph using bfs

---
+++ Graph/single_source_shortest_path_tree_using_bfs.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void defile()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif 
+}
+
+void addEdge(vector<int> a[],int from, int to);
+void showGraph(vector<int> a[],int V);
+void inputGraph(vector<int> a[]);
+void sssp(vector<int> a[],int V,int source = 0);
+
+int main() {
+	defile();
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int V,E;/*number of vertices and edges resp.*/
+	cin>>V>>E;
+	vector<int> a[V+1];
+	inputGraph(a);
+	showGraph(a,V);	
+
+	sssp(a,V);
+
+
+	return 0;
+}
+/*end of main------------------------------------->*/
+
+void sssp(vector<int> a[],int V,int source)
+{
+	bool visited[V]={false};
+	int paths[V] = {0};
+	queue<int> q;
+	q.push(source);
+	visited[source] = 1;
+
+	while(!q.empty())
+	{
+		int curr = q.front();
+		q.pop();
+
+		for(int x : a[curr])
+		{
+			if(!visited[x])
+			{
+				visited[x]=true;
+				q.push(x);
+				paths[x] = paths[curr]+1;
+			}
+		}
+	}
+
+	cout<<"Shortest path to source : "<<source<<endl;
+	for(int i=0;i<V;i++)
+	{
+		cout<<i<<" : "<<paths[i]<<endl;
+	}
+}
+
+
+
+void inputGraph(vector<int> a[])
+{
+	int from,to;
+	from =1;
+	to = 1;
+	while(from>=0&&to>=0)
+	{
+		cin>>from>>to;
+		if(from>=0&&to>=0)
+		addEdge(a,from,to);
+
+	}
+}
+
+/*for undirected graph*/
+void addEdge(vector<int> a[],int from, int to)
+{
+	a[from].push_back(to);
+	a[to].push_back(from);
+}
+
+void showGraph(vector<int> a[],int V)
+{
+	int i=0;
+	cout<<"Given Graph :--------> \n";
+	for(int i=0;i<V;i++)
+	{
+		cout<<i<<" : ";
+		for(int j = 0;j<a[i].size();j++)
+		{
+			cout<<a[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<"--------------------->"<<endl;
+
+}
